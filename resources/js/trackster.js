6d36c27d31e5d289a7fac8fb009be69c78467e83
Dario Dorella <dario.dorella@gmail.com>
2017-04-17 12:01:44 -0700

    Complete up to step 23 of the notes

--- resources/js/trackster.js
+++ resources/js/trackster.js
@@ -1,5 +1,33 @@
 var Trackster = {};
 
+$( document ).ready(function() {
+    console.log( "ready!" );
+
+    /*
+      Search button handler
+    */
+    $("#searchButton").click(function() {
+      console.log("#searchButton: .click()");
+      console.log("#searchInput content: '" + $('#searchInput').val() + "'");
+      if ($('#searchInput').val().trim() !== '') {
+        Trackster.searchTracksByTitle($('#searchInput').val())
+      }
+    });
+
+    /*
+      xButton handler: clears the input box
+    */
+    $("#xButton").click(function() {
+      console.log("#xButton: .click()");
+      $("#searchInput").val(null);
+      /*
+        Do not understand why this second one is necessary? Safari related bug?
+      */
+      $("#searchInput").val(null);
+      console.log("#searchInput content: '" + $('#searchInput').val() + "'");
+    });
+});
+
 /*
   Given an array of track data, create the HTML for a Bootstrap row for each.
   Append each "row" to the container in the body to display all tracks.
@@ -9,13 +37,13 @@ Trackster.renderTracks = function(tracks) {
   console.log("Redering tracks list...");
   for (var track in tracks) {
     var trackRow = '<div class="row" id="resultRow">' +
-    '<div class="col-xs-1 text-center"><a href="https://p.scdn.co/mp3-preview/22bf10aff02db272f0a053dff5c0063d729df988?cid=null"><i class="fa fa-play-circle-o fa-2x" aria-hidden="true"></i></a></div>' +
-    '<div class="col-xs-1 text-right"><span>' + track + '</span></div>'+
-    '<div class="col-xs-3"><span>' + tracks[track].name + '</span></div>'+
-    '<div class="col-xs-2"><span>' + tracks[track].album.artists[0].name + '</span></div>'+
-    '<div class="col-xs-3"><span>' + tracks[track].album.name + '</span></div>'+
-    '<div class="col-xs-1"><span>' + tracks[track].popularity + '</span></div>'+
-    '<div class="col-xs-1"><span>' + tracks[track].duration_ms + '</span></div></div>';
+    '<div class="col-xs-1 text-center"><a href="' + tracks[track].preview_url + '"><i class="fa fa-play-circle-o fa-2x" aria-hidden="true"></i></a></div>' +
+    '<div class="col-xs-1 text-right tableCell"><span>' + track + '</span></div>'+
+    '<div class="col-xs-3 tableCell"><span>' + tracks[track].name + '</span></div>'+
+    '<div class="col-xs-2 tableCell"><span>' + tracks[track].artists[0].name + '</span></div>'+
+    '<div class="col-xs-3 tableCell"><span>' + tracks[track].album.name + '</span></div>'+
+    '<div class="col-xs-1 tableCell"><span>' + tracks[track].popularity + '</span></div>'+
+    '<div class="col-xs-1 tableCell"><span>' + tracks[track].duration_ms + '</span></div></div>';
     $('#results').append(trackRow);
     console.log(tracks[track]);
   }
