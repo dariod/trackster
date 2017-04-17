aed3c0d24315fa8b3d2777a3b408f515b4174553
Dario Dorella <dario.dorella@gmail.com>
2017-04-17 15:41:02 -0700

    Animate logo on search

--- resources/js/trackster.js
+++ resources/js/trackster.js
@@ -1,36 +1,32 @@
 var Trackster = {};
 
 $( document ).ready(function() {
-    console.log( "ready!" );
-
     /*
       Search button handler
     */
     $("#searchInput").keypress(function(key) {
       if(key.which == 13) {
+        console.log("Pressed ENTER on #searchInput");
         $("#searchButton").click();
       }
     });
 
     $("#searchButton").click(function() {
-      console.log("#searchButton: .click()");
-      console.log("#searchInput content: '" + $('#searchInput').val() + "'");
       if ($('#searchInput').val().trim() !== '') {
-        Trackster.searchTracksByTitle($('#searchInput').val())
+        Trackster.searchTracksByTitle($('#searchInput').val());
       }
+
     });
 
     /*
       xButton handler: clears the input box
     */
     $("#xButton").click(function() {
-      console.log("#xButton: .click()");
       $("#searchInput").val(null);
       /*
         Do not understand why this second one is necessary? Safari related bug?
       */
       $("#searchInput").val(null);
-      console.log("#searchInput content: '" + $('#searchInput').val() + "'");
     });
 });
 
@@ -40,7 +36,6 @@ $( document ).ready(function() {
 */
 Trackster.renderTracks = function(tracks) {
   $('#results').empty();
-  console.log("Redering tracks list...");
   for (var track in tracks) {
     var trackRow = '<div class="row" id="resultRow">' +
     '<div class="col-xs-1 text-center"><a href="' + tracks[track].preview_url + '"><i class="fa fa-play-circle-o fa-2x" aria-hidden="true"></i></a></div>' +
@@ -51,7 +46,6 @@ Trackster.renderTracks = function(tracks) {
     '<div class="col-xs-1 tableCell"><span>' + tracks[track].popularity + '</span></div>'+
     '<div class="col-xs-1 tableCell"><span>' + tracks[track].duration_ms + '</span></div></div>';
     $('#results').append(trackRow);
-    console.log(tracks[track]);
   }
 };
 
@@ -63,8 +57,44 @@ Trackster.searchTracksByTitle = function(title) {
   $.ajax({
     url: "https://api.spotify.com/v1/search?type=track&q=" + title.replace(" ","+"),
     dataType: "json",
+    beforeSend: function () {
+      /*
+        Animate between logo and looking
+      */
+      $("#tracksterLogo").queue( function () {
+        $("#tracksterLogo").fadeOut(1000);
+        $(this).dequeue();
+      });
+      $("#tracksterLogo").queue( function () {
+        $("#tracksterLogo").html("<h1>L<i class=\"fa fa-eye\" aria-hidden=\"true\"></i><i class=\"fa fa-eye\" aria-hidden=\"true\"></i>king...</h1>");
+        $(this).dequeue();
+      });
+      $("#tracksterLogo").queue( function () {
+        $("#tracksterLogo").fadeIn(1000).delay(500);
+        $(this).dequeue();
+      });
+      },
     success: function (data) {
       Trackster.renderTracks(data["tracks"]["items"]);
+      },
+    complete: function () {
+      /*
+        Animate between looking and logo
+      */
+      $("#tracksterLogo").promise().done( function () {
+        $("#tracksterLogo").queue( function () {
+          $("#tracksterLogo").fadeOut(1000)
+          $(this).dequeue();
+        });
+        $("#tracksterLogo").queue( function () {
+          $("#tracksterLogo").html("<h1>trackster</h1>");
+          $(this).dequeue();
+        });
+        $("#tracksterLogo").queue( function () {
+          $("#tracksterLogo").fadeIn(1000);
+          $(this).dequeue();
+        });
+      });
     }
   });
 };
